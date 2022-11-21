6eb0e2a9b49fc6e1693082f5728d5931e5e77ca0
Alexsander Santiago <alexsander.santiago.2013@gmail.com>
2022-11-20 23:16:48 -0300

    add checagem quantidades de arestas e grafo nulo

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 from matriz_adjacencia import Grafo
 
 if __name__ == '__main__':
-    #Cricao de um grafo
     grafo = Grafo(int(input('Informe a quantidade de vértices de G: ')))
 
-    #Criacao de arestas
     grafo.criar_aresta(1, 2)
     grafo.criar_aresta(2, 3)
     grafo.criar_aresta(3, 4)
+    grafo.criar_aresta(4, 1)
 
-    #Remocao de arestas
-    #grafo.remover_aresta(1, 2)
-    #grafo.remover_aresta(3, 4)
+    # grafo.remover_aresta(1, 2)
+    # grafo.remover_aresta(3, 4)
 
     print('\nMatriz de adjacências definida:')
     grafo.obter_matriz()
 
-    #Checagem da existencia de arestas
-    print('\nChecagem da existência de aresta (1,2) ', grafo.checar_existencia_aresta(1, 2))
+    print('\nChecagem da existência de aresta (1,2): ', grafo.checar_existencia_aresta(1, 2))
 
-    #Checagem da quantidade de vertices e arestas
     print('\nChecagem da quantidade de vértices: ', grafo.checar_quantidade_vertices())
-    #print('\nChecagem da quantidade de arestas: ', grafo.checar_quantidade_arestas())
+    print('\nChecagem da quantidade de arestas: ', grafo.checar_quantidade_arestas())
+
+    print('\nChecagem de grafo nulo: ', grafo.checar_grafo_nulo())
--- matriz_adjacencia.py
+++ matriz_adjacencia.py
@@ -2,7 +2,7 @@ class Grafo:
 
     def __init__(self, vertices):
         self.vertices = vertices
-        self.grafo = [[0]*self.vertices for i in range(self.vertices)]
+        self.grafo = [[0] * self.vertices for i in range(self.vertices)]
 
     def criar_aresta(self, u, v):
         self.manipular_aresta(u, v, 1)
@@ -11,7 +11,10 @@ class Grafo:
         self.manipular_aresta(u, v, 0)
 
     def manipular_aresta(self, u, v, r):
-        self.grafo[u-1][v-1] = r
+        self.grafo[u - 1][v - 1] = r
+
+    def checar_grafo_nulo(self):
+        return self.checar_quantidade_arestas() == 0
 
     def checar_existencia_aresta(self, u, v):
         return self.obter_aresta(u, v) == 1
@@ -20,13 +23,16 @@ class Grafo:
         return self.vertices
 
     def checar_quantidade_arestas(self):
+        qtd_arestas = 0
         for i in range(self.vertices):
-            if self.grafo[i]==1:
-                print(self.grafo[i])
+            for y in self.grafo[i]:
+                if y == 1:
+                    qtd_arestas += 1
+        return qtd_arestas
 
     def obter_matriz(self):
         for i in range(self.vertices):
             print(self.grafo[i])
 
     def obter_aresta(self, u, v):
-        return self.grafo[u-1][v-1]
+        return self.grafo[u - 1][v - 1]
